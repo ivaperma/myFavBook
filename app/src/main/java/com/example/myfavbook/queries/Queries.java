9d7f59325f224401ae62b6a91a7f2579b788e02f
sergio2600 <sergioblazquez10@gmail.com>
2022-11-16 21:16:26 +0100

    Create Queries.java

---
+++ app/src/main/java/com/example/myfavbook/queries/Queries.java
@@ -0,0 +1,16 @@
+package com.example.myfavbook.queries;
+
+import android.widget.Toast;
+
+import com.example.myfavbook.activities.BookInfo;
+import com.example.myfavbook.activities.Search;
+import com.example.myfavbook.credentials.Login;
+
+public class Queries {
+
+
+    public void guardarFavorito (String usuario, BookInfo book){
+
+
+    }
+}
