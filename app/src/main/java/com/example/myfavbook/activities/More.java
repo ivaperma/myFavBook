8ea396d28720fd5a3e28f014d2fdd40b7ec232d2
sergio2600 <sergioblazquez10@gmail.com>
2022-11-23 13:38:29 +0100

    faq questions about app info

--- app/src/main/java/com/example/myfavbook/activities/More.java
+++ app/src/main/java/com/example/myfavbook/activities/More.java
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.myfavbook.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,6 +17,8 @@ import com.google.android.material.navigation.NavigationBarView;
 public class More extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    private TextView q1;
+    private TextView a2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +57,21 @@ public class More extends AppCompatActivity {
                         return true;
                 }
                 return false;
+            }
+        });
+
+        q1 = findViewById(R.id.qus1);
+        a2 = findViewById(R.id.ans2);
+        //initializing on click listner for our button.
+        q1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(a2.getVisibility()== View.GONE){
+                    a2.setVisibility(View.VISIBLE);
+                }
+                else{
+                    a2.setVisibility(View.GONE);
+                }
             }
         });
     }
